40c06bcb1f21d0108c49420e2318da12c802ab3b
Terry Lin <some.lin@gmail.com>
2016-05-11 09:22:37 +0800

    Create header.php

---
+++ views/header.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title><?= CI_title() ?></title>
+	<?= CI_head() ?>
+</head>
+<body>
